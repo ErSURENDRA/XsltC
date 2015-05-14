ed1cb5b4670d9207e9ae457ee87380f62dd17a40
ErSURENDRA <surendra.bisht023@gmail.com>
2015-05-14 06:27:11 +0530

    1

---
+++ XSLTCreator/XSLTCreator/FileReader.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+
+
+namespace XSLTCreator
+{
+    static class FileReader
+    {
+
+        
+        public static void GetXsltColumns(ref List<XsltColumn> XsltColumns, string path)
+        {
+
+           XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            String testing = doc.InnerXml;
+
+            var XmlDoc = testing.Split('<');
+            var XSDSimpleElements = XmlDoc.Where(str => str.StartsWith("xs:element") && str.EndsWith("/>"));
+
+            foreach (string value in XSDSimpleElements)
+            {
+                string value1 = value.Replace("name=\"", "'");
+                int startPos = value1.IndexOf("'") + 1;
+                int length = value1.IndexOf("\"") - startPos;
+
+                XsltColumn xsltcolumn = new XsltColumn();
+                xsltcolumn.Value = value1.Substring(startPos, length);
+
+                if (!XsltColumns.Contains(xsltcolumn))
+                    XsltColumns.Add(xsltcolumn);
+            }
+
+        }
+
+
+
+        public static  DataView ReadExcelFile(String ExcelPath)
+        {
+            try
+            {
+                string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + ExcelPath + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1\"";
+                if (ExcelPath.LastIndexOf("xlsx") > 0)
+                    strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + ExcelPath + ";Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1\"";
+
+                DataSet output = new DataSet();
+                using (OleDbConnection conn = new OleDbConnection(strConn))
+                {
+                    conn.Open();
+                    DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[]
+						{
+							null,
+							null,
+							null,
+							"TABLE"
+						});
+
+                    DataRow schemaRow = schemaTable.Rows[0];
+
+                    OleDbCommand cmd = new OleDbCommand("SELECT  * FROM [" + schemaRow["TABLE_NAME"].ToString() + "]", conn);
+                    cmd.CommandType = CommandType.Text;
+                    DataTable xlsfile = new DataTable(schemaRow["TABLE_NAME"].ToString());
+                    output.Tables.Add(xlsfile);
+                    new OleDbDataAdapter(cmd).Fill(xlsfile);
+                    if (xlsfile != null)
+                    {
+
+                        xlsfile.TableName = "PositionMaster";
+                        //  Converting XLS to Input/Serialized XML
+                        DataTable outputTable = xlsfile.Copy();
+                        for (int i = 0; i < outputTable.Columns.Count; i++)
+                            outputTable.Columns[i].ColumnName = "COL" + i;
+
+                        output.Tables.Clear();
+                        output.Tables.Add(outputTable);
+
+                        StreamWriter file = new StreamWriter(System.AppDomain.CurrentDomain.BaseDirectory + "\\serializedXML.xml");
+                        file.WriteLine(output.GetXml());
+                        file.Close();
+
+                        Dictionary<String, String> filecols = new Dictionary<string, string>();
+
+                        for (int i = 0; i < outputTable.Columns.Count; i++)
+                            filecols.Add(xlsfile.Columns[i].ColumnName, outputTable.Columns[i].ColumnName);
+
+                        CacheHandler.GetInstance().FileColumns = filecols;
+
+                        return xlsfile.DefaultView;
+
+                    }
+                    return null;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ XSLTCreator/XSLTCreator/MappingWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XSLTCreator
+{
+    /// <summary>
+    /// Interaction logic for MappingWindow.xaml
+    /// </summary>
+    public partial class MappingWindow : Window
+    {
+        MainWindow mainwindow;
+        List<XsltColumn> XsltColumns ;
+
+        public MappingWindow(Window mw)
+        {
+              
+            InitializeComponent();
+            mainwindow = mw as MainWindow;
+            this.Closed +=MappingWindow_Closed;
+            XsltColumns = new List<XsltColumn>();
+        }
+
+        private void MappingWindow_Closed(object sender, EventArgs e)
+        {
+            mainwindow.WindowState = WindowState.Normal;
+            mainwindow.w = null;
+        }
+
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                //HandleFileOpen(files[0]);
+                if (files[0].LastIndexOf(".xsd") > 0)
+                    FileReader.GetXsltColumns(ref XsltColumns,files[0]);
+                else
+                    MessageBox.Show("File not supported");
+
+                XsltColumns.Sort();
+                NeededColumns.ItemsSource = null;
+                NeededColumns.ItemsSource = XsltColumns;
+                CacheHandler.GetInstance().Listofcolumns = XsltColumns;
+            }
+        }
+    }
+}
